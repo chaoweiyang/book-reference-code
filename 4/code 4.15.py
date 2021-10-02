a488a6d47833536bd35aa85890da86c553a5ebe9
chaoweiyang <63939759+chaoweiyang@users.noreply.github.com>
2021-10-02 12:47:42 -0400

    Update code 4.15.py

--- 4/code 4.15.py
+++ 4/code 4.15.py
@@ -1,4 +1,4 @@
->>> f = open ('C:/Users/Phil/Downloads/points.txt', 'w+')
+>>> f = open ('C:/Users/Phil/Downloads/points.txt', 'w+') # please update the file path to where you want your file to be
 >>> f.write('point:\n')
 >>> f.write('p1: 1.0, 1.0 \n')
 >>> f.write('p2: 2.0, 2.0 \n')
