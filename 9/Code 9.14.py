b3d775868d59c0ecefbc614f2e8283d30b9ce3d0
chaoweiyang <63939759+chaoweiyang@users.noreply.github.com>
2021-10-02 13:43:22 -0400

    Update Code 9.14.py

--- 9/Code 9.14.py
+++ 9/Code 9.14.py
@@ -6,4 +6,4 @@ fieldlists = arcpy.ListFields("O:\\Book\\Code\\9\\chp9Data\\bookSampleData.gdb\\
 # observe each field in the returning list
 for field in fieldlists:
 	# print out the property of the field, its name, scale, and type
-	print field.name,field.scale,field.type
+	print(field.name,field.scale,field.type)
